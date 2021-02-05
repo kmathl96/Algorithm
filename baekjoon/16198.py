27f65e4150b6ef322f81e0846f36fa37f4bcd00d
kmathl96 <kmathl96@naver.com>
2021-02-06 02:59:58 +0900

    에너지 모으기 | 코드 수정 : 리스트 복사 -> insert

--- baekjoon/16198.py
+++ baekjoon/16198.py
@@ -1,5 +1,5 @@
 # 에너지 모으기
-# 브루트포스
+# 브루트 포스
 
 def f(arr,w): # 재귀함수
     global ans
@@ -7,13 +7,7 @@ def f(arr,w): # 재귀함수
         if ans < w: ans = w # 에너지값이 최대일 경우 갱신
         return
     for i in range(1,len(arr)-1):
-        tmp = arr[:]
-        tmp.pop(i) # 구슬 제거
+        p = arr.pop(i) # 구슬 제거
         # i번째 구슬을 제거했으므로 i-1번째, i번째 구슬의 곱을 현재 에너지에 더함
-        f(tmp,w+tmp[i-1]*tmp[i])
-
-int(input())
-arr = list(map(int, input().split()))
-ans = 0
-f(arr,0)
-print(ans)
+        f(arr,w+arr[i-1]*arr[i])
+        arr.insert(i,p) # 다음을 위해 제자리에 다시 구슬 넣기
