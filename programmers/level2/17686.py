b2b617db977cbe9aa4058b7f3775f478087d1c2d
kmathl96 <kmathl96@naver.com>
2021-06-03 03:00:53 +0900

    2018 KAKAO BLIND RECRUITMENT | [3차] 파일명 정렬

--- programmers/level2/17686.py
+++ programmers/level2/17686.py
@@ -25,4 +25,5 @@ def solution(files):
 
 print(solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"])) # ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
 print(solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"])) # ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
+
 print(solution(["foo9.txt", "foo010bar020.zip", "F-15", "F15"])) # ['F15', 'F-15', 'foo9.txt', 'foo010bar020.zip']
