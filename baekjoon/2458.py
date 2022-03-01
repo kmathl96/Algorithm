4dbb3a9ebd9eb9b37a19a2a8e6f56df4a641d724
kmathl96 <kmathl96@naver.com>
2022-03-01 14:00:54 +0900

    키 순서 | 플로이드-와샬 | 실행 속도 단축

--- baekjoon/2458.py
+++ baekjoon/2458.py
@@ -5,27 +5,24 @@ import sys
 input = sys.stdin.readline
 
 N,M = map(int,input().split()) # 학생들의 수, 두 학생 키를 비교한 횟수
-inf = float('INF')
-d = [[inf]*N for _ in range(N)]
+d = [[0]*N for _ in range(N)] # 두 학생이 연결되어 있는지 여부
 for _ in range(M):
     a,b = map(int,input().split()) # 두 학생의 키
     d[a-1][b-1] = 1 # a가 b보다 작음
 
 # 플로이드-와샬
 for k in range(N):
+    # i-k, k-j가 연결되어 있다면, i-j도 연결되어 있는 것임
     for i in range(N):
+        if not d[i][k]: continue # i-k가 연결되어 있지 않으면 넘어감
         for j in range(N):
-            d[i][j] = min(d[i][j],d[i][k]+d[k][j])
+            # i-j가 아직 연결되어 있지 않고 i-k, k-j가 연결되어 있는 경우
+            if not d[i][j] and d[k][j]:
+                d[i][j] = 1 # 연결 여부 체크
 
-ans = 0 # 자신의 키가 몇 번째인지 알 수 있는 학생의 수
-for i in range(N):
-    cnt = 0 # 본인과 키 우열을 가릴 수 있는 학생의 수
-    for j in range(N):
-        # 본인보다 키가 크거나 작은 경우, 우열을 가릴 수 있음
-        if d[i][j] != inf or  d[j][i] != inf:
-            cnt += 1
-    
-    # (N-1)명과 우열을 가릴 수 있다면 본인의 키 순서를 알 수 있음
-    if cnt==N-1:
-        ans += 1
-print(ans)
+# 연결되어있는 학생의 수를 계산
+# 1. i행에 연결 표시 된 학생은 i 학생보다 키가 작고
+# 2. i열에 연결 표시 된 학생은 i 학생보다 키가 큼
+# => 행과 열에 (N-1)명과 연결되어 있으면, 자신의 키가 몇 번째인지 알 수 있음
+# => (N-1)명과 연결되어 있는 학생의 수 출력
+print([sum(row)+sum(col) for row,col in zip(d,zip(*d))].count(N-1))
